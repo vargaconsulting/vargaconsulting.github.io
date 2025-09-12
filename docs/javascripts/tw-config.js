2d2d784c97a73791884d47586c2198a31db3d0f7
svarga <steven@vargaconsulting.ca>
2025-09-12 07:04:58 -0400

    [#28]:svarga:site, inline header icons and replace theme switcher

--- docs/javascripts/tw-config.js
+++ docs/javascripts/tw-config.js
@@ -2,3 +2,11 @@
 tailwind.config = {
   corePlugins: { preflight: false },
 };
+// tailwind.config.js
+module.exports = {
+  content: ["./docs/**/*.md", "./overrides/**/*.html"],
+  // Treat either .dark class or MkDocs Material's slate scheme as "dark"
+  darkMode: ["class", '[data-md-color-scheme="slate"]'],
+  theme: { extend: {} },
+  plugins: [],
+};
