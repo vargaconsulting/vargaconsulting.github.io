1db8897f0b95e95dc9b1ab44382b50fac265e50c
svarga <steven@vargaconsulting.ca>
2025-10-07 16:52:34 -0400

    [#37]:svarga:site, adopting custom compiled asciinema style

--- docs/javascripts/tw-config.js
+++ docs/javascripts/tw-config.js
@@ -1,11 +1,5 @@
-// Keep Material’s base styles intact
 tailwind.config = {
   corePlugins: { preflight: false },
-};
-// tailwind.config.js
-module.exports = {
-  content: ["./docs/**/*.md", "./overrides/**/*.html"],
-  // Treat either .dark class or MkDocs Material's slate scheme as "dark"
   darkMode: ["class", '[data-md-color-scheme="slate"]'],
   theme: { extend: {} },
   plugins: [],
